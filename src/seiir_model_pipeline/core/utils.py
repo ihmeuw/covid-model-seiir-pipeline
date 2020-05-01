f845b0ab1d530f810b795b28159d96a423de0b6c
zhengp0 <zhengp@uw.edu>
2020-05-01 09:53:52 -0700

    change COL_T to COL_DATE.

--- src/seiir_model_pipeline/core/utils.py
+++ src/seiir_model_pipeline/core/utils.py
@@ -9,7 +9,6 @@ from slime.core.data import MRData
 
 SEIIR_COMPARTMENTS = ['S', 'E', 'I1', 'I2', 'R']
 
-COL_T = 'days'
 COL_BETA = 'beta'
 COL_GROUP = 'loc_id'
 COL_DATE = 'date'
@@ -68,10 +67,10 @@ def convert_inputs_for_beta_model(data_cov, df_beta, covmodel_set):
     df_cov, col_t_cov, col_group_cov = data_cov
     df = df_beta.merge(
         df_cov,
-        left_on=[COL_T, COL_GROUP],
+        left_on=[COL_DATE, COL_GROUP],
         right_on=[col_t_cov, col_group_cov],
     ).copy()
-    df.sort_values(inplace=True, by=[COL_GROUP, COL_T])
+    df.sort_values(inplace=True, by=[COL_GROUP, COL_DATE])
     cov_names = [covmodel.col_cov for covmodel in covmodel_set.cov_models]
     mrdata = MRData(df, col_group=COL_GROUP, col_obs=COL_BETA, col_covs=cov_names)
 
