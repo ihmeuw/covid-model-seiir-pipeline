33db011000feb81d6b7963a8e5f125c8188f3dfe
Marlena Bannick <mnorwood@uw.edu>
2020-05-01 13:46:54 -0700

    forecasting

--- src/seiir_model_pipeline/executor/beta_forecast.py
+++ src/seiir_model_pipeline/executor/beta_forecast.py
@@ -3,14 +3,16 @@ import logging
 from typing import List
 
 from seiir_model.model_runner import ModelRunner
+from seiir_model.ode_forecasting.ode_runner import SiierdModelSpecs
 
 from seiir_model_pipeline.core.versioner import args_to_directories
 from seiir_model_pipeline.core.versioner import load_forecast_settings, load_regression_settings
 from seiir_model_pipeline.core.versioner import PEAK_DATE_FILE, PEAK_DATE_COL_DICT, COVARIATE_COL_DICT, OBSERVED_DICT
-from seiir_model_pipeline.core.data import load_covariates
+from seiir_model_pipeline.core.data import load_covariates, load_beta_fit, load_beta_params
 from seiir_model_pipeline.core.data import load_mr_coefficients
 from seiir_model_pipeline.core.utils import convert_to_covmodel
 from seiir_model_pipeline.core.versioner import OBSERVED_DICT
+from seiir_model_pipeline.core.utils import get_ode_init_cond
 
 log = logging.getLogger(__name__)
 
@@ -37,6 +39,10 @@ def main():
     directories = args_to_directories(args)
     regression_settings = load_regression_settings(directories)
     forecast_settings = load_forecast_settings(directories)
+
+    mr = ModelRunner()
+
+    # Get all inputs for the beta forecasting
     covmodel_set = convert_to_covmodel(regression_settings.covariates)
     covariate_data = load_covariates(
         directories,
@@ -45,16 +51,40 @@ def main():
         col_observed=COVARIATE_COL_DICT['COL_OBSERVED'],
         forecasted=OBSERVED_DICT['forecasted']
     )
-    mr = ModelRunner()
     regression_fit = load_mr_coefficients(
         directories=directories,
         location_id=args.location_id,
         draw_id=args.draw_id
     )
-    mr.predict_beta_forward(
+    forecasts = mr.predict_beta_forward(
         covmodel_set=covmodel_set,
         df_cov=covariate_data,
         col_t=COVARIATE_COL_DICT['COL_DATE'],
         col_group=COVARIATE_COL_DICT['COL_LOC_ID']
+    )
+
+    # Get all inputs for the ODE
+    beta_fit = load_beta_fit(
+        directories, draw_id=args.draw_id,
+        location_id=args.location_id
+    )
+    beta_params = load_beta_params(
+        directories, draw_id=args.draw_id
+    )
+    init_cond = get_ode_init_cond(
+        beta_ode_fit=beta_fit,
+        current_date=covariate_data[COVARIATE_COL_DICT['COL_DATE']].min()
+    )
+    model_specs = SiierdModelSpecs(
+        alpha=beta_params['alpha'],
+        sigma=beta_params['beta']
+    )
+    times = None
+    mr.forecast(
+        model_specs=model_specs,
+        init_cond=None,
+        times=times,
+        betas=forecasts,
+        dt=regression_settings.solver_dt
     )
 
