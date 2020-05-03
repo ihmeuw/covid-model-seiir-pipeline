8be9601c6c5467c883d06204062ec29296683a87
mnorwood <mnorwood@uw.edu>
2020-05-03 13:03:58 -0700

    mISSING LOCS

--- src/seiir_model_pipeline/executor/create_diagnostics.py
+++ src/seiir_model_pipeline/executor/create_diagnostics.py
@@ -39,6 +39,7 @@ def main():
     location_ids = get_locations(
         directories, settings.location_set_version_id
     )
+    location_ids = [x for x in location_ids if x not in [60407, 60406, 60405]]
 
     visualizer = Visualizer(
         directories, groups=location_ids,
