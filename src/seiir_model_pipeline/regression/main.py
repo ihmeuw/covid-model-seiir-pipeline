272252280b13138c0880c0ba60e1f43f05241460
Logan Sandar <mlsandar@uw.edu>
2020-05-20 13:56:50 -0700

    a few more changes

--- src/seiir_model_pipeline/regression/main.py
+++ src/seiir_model_pipeline/regression/main.py
@@ -2,13 +2,33 @@ from pathlib import Path
 
 from covid_shared import cli_tools
 
-from seiir_model_pipeline.regression.specification import RegressionSpecification, validate_specification
+from seiir_model_pipeline.exceptions import VersionAlreadyExists
+from seiir_model_pipeline.regression.specification import (RegressionSpecification,
+                                                           validate_specification)
+from seiir_model_pipeline.regression.version import RegressionVersion
+
+
+# TODO: add to cli
+resume = False
 
 
 def do_beta_regression(app_metadata: cli_tools.Metadata,
                        regression_specification: RegressionSpecification,
-                       infection_root: Path,
-                       covariates_root: Path,
-                       output_root: Path):
+                       output_dir: Path):
     validate_specification(regression_specification)
-    pass
+
+    try:
+        regress_version = RegressionVersion.init_version(
+            regression_specification=regression_specification,
+            regression_dir=output_dir
+        )
+    except VersionAlreadyExists:
+        if resume:
+            regress_version = RegressionVersion(
+                regression_specification=regression_specification,
+                regression_dir=output_dir
+            )
+        else:
+            raise
+
+    regress_version.run()
