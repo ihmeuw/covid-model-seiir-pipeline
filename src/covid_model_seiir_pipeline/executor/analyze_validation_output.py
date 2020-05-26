05b18613660158e38ad686ceac6b3c4f768065e9
Marlena Bannick <mnorwood@uw.edu>
2020-05-26 12:13:06 -0700

    Evaluating location with days rather than date.

--- src/covid_model_seiir_pipeline/executor/analyze_validation_output.py
+++ src/covid_model_seiir_pipeline/executor/analyze_validation_output.py
@@ -12,6 +12,7 @@ from covid_model_seiir_pipeline.core.versioner import INFECTION_COL_DICT
 log = logging.getLogger(__name__)
 
 ALL_METRICS_FILE = 'ALL_metrics.csv'
+COL_DAYS = 'days'
 
 
 def parse_arguments(argstr: Optional[str] = None) -> Namespace:
@@ -28,25 +29,32 @@ def parse_arguments(argstr: Optional[str] = None) -> Namespace:
     return args
 
 
+def date_to_days(date):
+    date = pd.to_datetime(date)
+    return np.array((date - date.min()).days) + 1
+
+
 def load_all_validation_data(output_dir):
     dfs = []
     for file in os.listdir(output_dir):
         if file == ALL_METRICS_FILE:
             continue
         df = pd.read_csv(output_dir / file)
+        df[COL_DAYS] = date_to_days(df[INFECTION_COL_DICT['COL_DATE']])
+        df.drop(columns=INFECTION_COL_DICT['COL_DATE'], inplace=True, axis=1)
         dfs.append(df)
     return pd.concat(dfs).reset_index()
 
 
 def collapse_metrics(df):
-    groups = df.groupby(INFECTION_COL_DICT['COL_DATE'])
+    groups = df.groupby('days')
 
     rmse = groups.sq_error.apply(lambda x: np.sqrt(np.mean(x))).values
     bias = groups.bias.mean().values
     coverage = groups.coverage.mean().values
 
     result = pd.DataFrame({
-        INFECTION_COL_DICT['COL_DATE']: list(groups.groups.keys()),
+        COL_DAYS: list(groups.groups.keys()),
         'rmse': rmse,
         'bias': bias,
         'coverage': coverage
