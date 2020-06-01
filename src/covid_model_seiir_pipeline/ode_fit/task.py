31c169d0df49a0f245496d6997a485c39547509a
James Collins <collijk@uw.edu>
2020-06-01 12:58:57 -0700

    Fix interface drift

--- src/covid_model_seiir_pipeline/ode_fit/task.py
+++ src/covid_model_seiir_pipeline/ode_fit/task.py
@@ -21,15 +21,22 @@ log = logging.getLogger(__name__)
 def run_ode_fit(draw_id: int, ode_version: str):
     # -------------------------- LOAD INPUTS -------------------- #
     # Load metadata
-    ode_fit_spec: FitSpecification = FitSpecification.from_path(
+    fit_specification: FitSpecification = FitSpecification.from_path(
         Path(ode_version) / "fit_specification.yaml"
     )
-    ode_data_interface = ODEDataInterface(ode_fit_spec.data)
+    data_interface = ODEDataInterface(
+        ode_fit_root=Path(fit_specification.data.output_root),
+        infection_root=Path(fit_specification.data.infection_version),
+        location_file=(
+                Path('/ihme/covid-19/seir-pipeline-outputs/metadata-inputs') /
+                f'location_metadata_{fit_specification.data.location_set_version_id}.csv'
+        )
+    )
 
     # Load data
-    location_ids = ode_data_interface.load_location_ids()
-    location_data = ode_data_interface.load_all_location_data(location_ids=location_ids,
-                                                              draw_id=draw_id)
+    location_ids = data_interface.load_location_ids()
+    location_data = data_interface.load_all_location_data(location_ids=location_ids,
+                                                          draw_id=draw_id)
 
     # This seed is so that the alpha, sigma, gamma1 and gamma2 parameters are reproducible
     np.random.seed(draw_id)
@@ -42,23 +49,23 @@ def run_ode_fit(draw_id: int, ode_version: str):
         col_loc_id=INFECTION_COL_DICT['COL_LOC_ID'],
         col_lag_days=INFECTION_COL_DICT['COL_ID_LAG'],
         col_observed=INFECTION_COL_DICT['COL_OBS_DEATHS'],
-        alpha=ode_fit_spec.parameters.alpha,
-        sigma=ode_fit_spec.parameters.sigma,
-        gamma1=ode_fit_spec.parameters.gamma1,
-        gamma2=ode_fit_spec.parameters.gamma2,
-        solver_dt=ode_fit_spec.parameters.solver_dt,
+        alpha=fit_specification.parameters.alpha,
+        sigma=fit_specification.parameters.sigma,
+        gamma1=fit_specification.parameters.gamma1,
+        gamma2=fit_specification.parameters.gamma2,
+        solver_dt=fit_specification.parameters.solver_dt,
         spline_options={
-            'spline_knots': ode_fit_spec.parameters.knots,
-            'spline_degree': ode_fit_spec.parameters.degree,
-            'prior_spline_convexity': None if not ode_fit_spec.parameters.concavity else 'concave',
-            'prior_spline_monotonicity': None if not ode_fit_spec.parameters.increasing else 'increasing',
-            'spline_knots_type': ode_fit_spec.parameters.spline_knots_type,
-            'spline_r_linear': ode_fit_spec.parameters.spline_r_linear,
-            'spline_l_linear': ode_fit_spec.parameters.spline_l_linear,
+            'spline_knots': fit_specification.parameters.knots,
+            'spline_degree': fit_specification.parameters.degree,
+            'prior_spline_convexity': None if not fit_specification.parameters.concavity else 'concave',
+            'prior_spline_monotonicity': None if not fit_specification.parameters.increasing else 'increasing',
+            'spline_knots_type': fit_specification.parameters.spline_knots_type,
+            'spline_r_linear': fit_specification.parameters.spline_r_linear,
+            'spline_l_linear': fit_specification.parameters.spline_l_linear,
         },
-        day_shift=ode_fit_spec.parameters.day_shift,
-        spline_se_power=ode_fit_spec.parameters.spline_se_power,
-        spline_space=ode_fit_spec.parameters.spline_space,
+        day_shift=fit_specification.parameters.day_shift,
+        spline_se_power=fit_specification.parameters.spline_se_power,
+        spline_space=fit_specification.parameters.spline_space,
     )
 
     # ----------------------- BETA SPLINE + ODE -------------------------------- #
@@ -70,13 +77,13 @@ def run_ode_fit(draw_id: int, ode_version: str):
     beta_fit = mr.get_beta_ode_fit()
     for l_id in location_ids:
         loc_beta_fits = beta_fit.loc[beta_fit[INFECTION_COL_DICT['COL_LOC_ID']] == l_id].copy()
-        ode_data_interface.save_draw_beta_fit_file(loc_beta_fits, l_id, draw_id)
+        data_interface.save_draw_beta_fit_file(loc_beta_fits, l_id, draw_id)
 
     # Save the parameters of alpha, sigma, gamma1, and gamma2 that were drawn
     draw_beta_params = mr.get_beta_ode_params()
     beta_start_end_dates = mr.get_beta_start_end_dates()
-    ode_data_interface.save_draw_beta_param_file(draw_beta_params, draw_id)
-    ode_data_interface.save_draw_date_file(beta_start_end_dates, draw_id)
+    data_interface.save_draw_beta_param_file(draw_beta_params, draw_id)
+    data_interface.save_draw_date_file(beta_start_end_dates, draw_id)
 
 
 def parse_arguments(argstr: Optional[str] = None) -> Namespace:
