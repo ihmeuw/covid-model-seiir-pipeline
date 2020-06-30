8d166d9a94512585514a33e66da13c642ea968d9
Megan Moore <mm7148@uw.edu>
2020-06-30 15:20:44 -0700

    changed to allow for theta mapping so more than two options can be done

--- src/covid_model_seiir_pipeline/core/versioner.py
+++ src/covid_model_seiir_pipeline/core/versioner.py
@@ -387,26 +387,20 @@ class ForecastVersion(Version):
     covariate_draw_dict: Dict[str, bool]
 
     theta: int = field(default=None)
-    theta_plus: int = field(default=None)
-    theta_minus: int = field(default=None)
-    theta_minus_locations_file: str = field(default=None)
+    theta_locations_file: str = field(default=None)
 
     def __post_init__(self):
         # TODO: Don't just fail fast.  fail helpfully
         if self.theta is not None:
-            assert self.theta_plus is None
-            assert self.theta_minus is None
-            assert self.theta_minus_locations_file is None
-        elif self.theta_minus is None and self.theta_plus is None and self.theta_minus_locations_file is None:
+            assert self.theta_locations_file is None
+        elif self.theta_locations_file is None:
             # Default case, no config provided
             self.theta = 0
         else:
-            assert isinstance(self.theta_plus, int) and self.theta_plus >= 0
-            assert isinstance(self.theta_minus, int) and self.theta_minus < 0
-            theta_minus_loc_file = Path(self.theta_minus_locations_file)
-            assert (theta_minus_loc_file.exists()
-                    and theta_minus_loc_file.is_file()
-                    and theta_minus_loc_file.suffix == '.csv')
+            theta_loc_file = Path(self.theta_locations_file)
+            assert (theta_loc_file.exists()
+                    and theta_loc_file.is_file()
+                    and theta_loc_file.suffix == '.csv')
 
     def _settings_to_json(self):
         settings = asdict(self)
