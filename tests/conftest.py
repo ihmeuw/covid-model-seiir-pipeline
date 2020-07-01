9aae45b03c7efa0f4458e08ebd970f1c382df350
Mike Richards <miker985@uw.edu>
2020-07-01 12:24:23 -0700

    Refactor reusable file counter to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 
 import pytest
@@ -7,3 +8,16 @@ import pytest
 def fixture_dir():
     here = pathlib.Path(__file__).parent
     return here / "fixtures"
+
+
+@pytest.fixture
+def tmpdir_file_count(tmpdir):
+    def file_count():
+        "Return count of files in our ODEDataInterface storage location."
+        cnt = 0
+        for _, _, files in os.walk(tmpdir):
+            for _ in files:
+                cnt += 1
+        return cnt
+
+    return file_count
