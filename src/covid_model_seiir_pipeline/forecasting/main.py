3a0b3f314e452da1701a223b1e3a2fb58d50605f
James Collins <collijk@uw.edu>
2020-07-01 11:21:51 -0700

    Carve out forecast implementation for now

--- src/covid_model_seiir_pipeline/forecasting/main.py
+++ src/covid_model_seiir_pipeline/forecasting/main.py
@@ -2,50 +2,37 @@ from pathlib import Path
 
 from covid_shared import cli_tools
 
-from covid_model_seiir_pipeline import static_vars
-
 from covid_model_seiir_pipeline.paths import ForecastPaths
 from covid_model_seiir_pipeline.forecasting.specification import ForecastSpecification
 from covid_model_seiir_pipeline.forecasting.data import ForecastDataInterface
 from covid_model_seiir_pipeline.forecasting.workflow import ForecastWorkflow
 
-from covid_model_seiir_pipeline.ode_fit.specification import FitSpecification
-from covid_model_seiir_pipeline.regression.specification import RegressionSpecification
-
 
 def do_beta_forecast(app_metadata: cli_tools.Metadata,
-                     forecast_specification: ForecastSpecification,
-                     output_root: Path):
-    regression_specification: RegressionSpecification = RegressionSpecification.from_path(
-        Path(forecast_specification.data.regression_version) /
-        static_vars.REGRESSION_SPECIFICATION_FILE
-    )
-    ode_fit_spec: FitSpecification = FitSpecification.from_path(
-        Path(regression_specification.data.ode_fit_version) /
-        static_vars.FIT_SPECIFICATION_FILE
-    )
+                     forecast_specification: ForecastSpecification):
 
-    for scenario in forecast_specification.scenarios.keys():
-        scenario_root = output_root / scenario
-
-        # init high level objects
-        forecast_paths = ForecastPaths(scenario_root, read_only=False)
-        data_interface = ForecastDataInterface(
-            forecast_root=scenario_root,
-            regression_root=Path(regression_specification.data.output_root),
-            ode_fit_root=Path(ode_fit_spec.data.output_root),
-            infection_root=Path(ode_fit_spec.data.infection_version),
-            location_file=Path(ode_fit_spec.data.location_set_file)
-        )
-
-        # build directory structure
-        location_ids = data_interface.load_location_ids()
-        forecast_paths.make_dirs(location_ids)
+    return
+    # for scenario in forecast_specification.scenarios.keys():
+    #     scenario_root = output_root / scenario
+    #
+    #     # init high level objects
+    #     forecast_paths = ForecastPaths(scenario_root, read_only=False)
+    #     data_interface = ForecastDataInterface(
+    #         forecast_root=scenario_root,
+    #         regression_root=Path(regression_specification.data.output_root),
+    #         ode_fit_root=Path(ode_fit_spec.data.output_root),
+    #         infection_root=Path(ode_fit_spec.data.infection_version),
+    #         location_file=Path(ode_fit_spec.data.location_set_file)
+    #     )
+    #
+    #     # build directory structure
+    #     location_ids = data_interface.load_location_ids()
+    #     forecast_paths.make_dirs(location_ids)
 
     # build workflow and launch
-    forecast_wf = ForecastWorkflow(forecast_specification, location_ids)
-    forecast_wf.attach_scenario_tasks()
-    forecast_wf.attach_splice_tasks()
-    forecast_wf.attach_scaling_diagnostics_tasks()
-    forecast_wf.attach_forecast_diagnostics_tasks()
-    forecast_wf.run()
+    # forecast_wf = ForecastWorkflow(forecast_specification, location_ids)
+    # forecast_wf.attach_scenario_tasks()
+    # forecast_wf.attach_splice_tasks()
+    # forecast_wf.attach_scaling_diagnostics_tasks()
+    # forecast_wf.attach_forecast_diagnostics_tasks()
+    # forecast_wf.run()
