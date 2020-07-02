4636be860b2c0a9adce4d70e490caa09911fcd12
Mike Richards <miker985@uw.edu>
2020-07-02 09:30:37 -0700

    Add ZipMarshall + tests

--- tests/test_marshall.py
+++ tests/test_marshall.py
@@ -6,6 +6,7 @@ import pytest
 from covid_model_seiir_pipeline.marshall import (
     Keys,
     CSVMarshall,
+    ZipMarshall,
 )
 
 
@@ -13,6 +14,22 @@ class MarshallInterfaceTests:
     """
     Mixin class for testing the marshall interface.
     """
+    def test_beta_marshall(self, instance, fit_beta):
+        self.assert_load_dump_workflow_correct(instance, fit_beta, key=Keys.fit_beta(4))
+
+    def test_parameters_marshall(self, instance, parameters):
+        self.assert_load_dump_workflow_correct(instance, parameters, key=Keys.parameter(4))
+
+    def test_date_marshall(self, instance, dates):
+        self.assert_load_dump_workflow_correct(instance, dates, key=Keys.date(4))
+
+    def test_coefficients_marshall(self, instance, coefficients):
+        self.assert_load_dump_workflow_correct(instance, coefficients, key=Keys.coefficient(4))
+
+    def test_no_overwriting(self, instance, fit_beta, parameters):
+        self.assert_no_accidental_overwrites(instance, fit_beta, key=Keys.fit_beta(4))
+        self.assert_no_accidental_overwrites(instance, parameters, key=Keys.parameter(4))
+
     def test_interface_methods(self, instance):
         "Test mandatory interface methods exist."
         assert hasattr(instance, "dump")
@@ -38,18 +55,9 @@ class TestCSVMarshall(MarshallInterfaceTests):
     def instance(self, tmpdir):
         return CSVMarshall(pathlib.Path(tmpdir))
 
-    def test_beta_marshall(self, instance, fit_beta):
-        self.assert_load_dump_workflow_correct(instance, fit_beta, key=Keys.fit_beta(4))
 
-    def test_parameters_marshall(self, instance, parameters):
-        self.assert_load_dump_workflow_correct(instance, parameters, key=Keys.parameter(4))
-
-    def test_date_marshall(self, instance, dates):
-        self.assert_load_dump_workflow_correct(instance, dates, key=Keys.date(4))
-
-    def test_coefficients_marshall(self, instance, coefficients):
-        self.assert_load_dump_workflow_correct(instance, coefficients, key=Keys.coefficient(4))
-
-    def test_no_overwriting(self, instance, fit_beta, parameters):
-        self.assert_no_accidental_overwrites(instance, fit_beta, key=Keys.fit_beta(4))
-        self.assert_no_accidental_overwrites(instance, parameters, key=Keys.parameter(4))
+class TestZipMarshall(MarshallInterfaceTests):
+    @pytest.fixture
+    def instance(self, tmpdir):
+        zip_path = str(tmpdir / "data.zip")
+        return ZipMarshall(zip_path)
