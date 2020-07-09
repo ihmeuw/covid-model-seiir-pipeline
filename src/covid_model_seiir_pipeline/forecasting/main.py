5964a6e5c1aab2291fb99e4a46f4668b977ddde2
James Collins <collijk@uw.edu>
2020-07-08 22:45:49 -0700

    Check covariates match regression

--- src/covid_model_seiir_pipeline/forecasting/main.py
+++ src/covid_model_seiir_pipeline/forecasting/main.py
@@ -11,6 +11,11 @@ from covid_model_seiir_pipeline.forecasting.workflow import ForecastWorkflow
 def do_beta_forecast(app_metadata: cli_tools.Metadata,
                      forecast_specification: ForecastSpecification):
     data_interface = ForecastDataInterface.from_specification(forecast_specification)
+
+    # Check scenario covariates the same as regression covariates and that
+    # covariate data versions match.
+    data_interface.check_covariates(forecast_specification.covariates)
+
     data_interface.make_dirs()
 
     return
