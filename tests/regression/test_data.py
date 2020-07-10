0adebe3348530ce06dadce90e7cef388fcf9e2a4
Mike Richards <miker985@uw.edu>
2020-07-10 12:07:08 -0700

    Cleanup work to align with no fit-less stage. Migrate tests from fit to regression; delete fit tests; update one save key (betas => beta)

--- tests/regression/test_data.py
+++ tests/regression/test_data.py
@@ -1,48 +1,14 @@
 from pathlib import Path
 
-import pandas
-import pytest
-
 from covid_model_seiir_pipeline.marshall import (
     CSVMarshall,
-    Keys as MKeys,
 )
 from covid_model_seiir_pipeline.paths import RegressionPaths
 from covid_model_seiir_pipeline.regression.data import RegressionDataInterface
 
 
 class TestRegressionDataInterfaceIO:
-    @pytest.mark.skip(reason='ODEPaths no longer exists')
-    def test_ode_fit_io(self, tmpdir, tmpdir_file_count, fit_beta):
-        """
-        Test I/O relating to the fit stage.
-
-        This only includes loading files.
-        """
-        ode_paths = ODEPaths(Path(tmpdir))
-        di = RegressionDataInterface(
-            regression_paths=None,
-            ode_paths=ode_paths,
-            covariate_paths=None,
-            ode_marshall=CSVMarshall(ode_paths.root_dir),
-            regression_marshall=None,
-        )
-
-        # Step 1: create files
-        m = CSVMarshall(di.ode_paths.root_dir)
-        m.dump(fit_beta, key=MKeys.fit_beta(draw_id=4))
-
-        # Step 2: load files
-        # Note: 10 corresponds to a value in the ode_fit_beta fixture
-        loaded = di.load_ode_fits(draw_id=4, location_ids=[10])
-
-        # Step 3: test
-        # Note: load_ode_fits explicitly converts the "date" column
-        assert pandas.core.dtypes.common.is_datetime64_any_dtype(loaded['date'])
-        loaded['date'] = loaded['date'].dt.strftime("%Y-%m-%d")
-        pandas.testing.assert_frame_equal(fit_beta, loaded)
-
-    def test_regression_io(self, tmpdir, tmpdir_file_count, coefficients, regression_beta):
+    def test_regression_io(self, tmpdir, tmpdir_file_count, parameters, dates, coefficients, regression_beta):
         """
         Test I/O relating to regression stage.
 
@@ -56,9 +22,14 @@ class TestRegressionDataInterfaceIO:
             regression_marshall=CSVMarshall(regress_paths.root_dir),
         )
         # Step 1: count files
+
         assert tmpdir_file_count() == 0, "Files somehow already exist in storage dir"
+
         # Step 2: save files
+        di.save_draw_beta_param_file(parameters, draw_id=4)
+        di.save_draw_date_file(dates, draw_id=4)
         di.save_regression_coefficients(coefficients, draw_id=4)
         di.save_regression_betas(regression_beta, draw_id=4)
+
         # Step 3: count files (again)
-        assert tmpdir_file_count() == 2
+        assert tmpdir_file_count() == 4
