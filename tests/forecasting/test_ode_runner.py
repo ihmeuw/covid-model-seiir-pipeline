ac83b5f7460185217f6b7c2fbc0810433fa092e5
James Collins <collijk@uw.edu>
2020-07-14 16:41:41 -0700

    Get tests passing

--- tests/forecasting/test_ode_runner.py
+++ tests/forecasting/test_ode_runner.py
@@ -10,13 +10,13 @@ def test_ode_runner():
         gamma1=0.3,
         gamma2=0.4,
         N=100,
+        delta=0.1,
     )
     # init_cond = [S, E, I1, I2, R] at t = 0
     init_cond = np.array([96, 0, 2, 2, 0], dtype=float)
 
     t = np.arange(0, 31, 1)
-    dt = 0.1  # for the ODE solver, so it can be less than a day
     beta = 2*np.exp(-0.01*t)
-    ode_runner = ODERunner(specs, init_cond, dt)
+    ode_runner = ODERunner(specs, init_cond)
     result = ode_runner.get_solution(t, beta)
     print("Okay!")
