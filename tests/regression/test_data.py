a90a78f418009f3bfa8daa9dfe10da428fbc98d6
Mike Richards <miker985@uw.edu>
2020-07-20 14:00:03 -0700

    Update tested methods for new API; warn on near-exact comparison

--- tests/regression/test_data.py
+++ tests/regression/test_data.py
@@ -26,8 +26,8 @@ class TestRegressionDataInterfaceIO:
         assert tmpdir_file_count() == 0, "Files somehow already exist in storage dir"
 
         # Step 2: save files
-        di.save_draw_beta_param_file(parameters, draw_id=4)
-        di.save_draw_date_file(dates, draw_id=4)
+        di.save_beta_param_file(parameters, draw_id=4)
+        di.save_date_file(dates, draw_id=4)
         di.save_regression_coefficients(coefficients, draw_id=4)
         di.save_regression_betas(regression_beta, draw_id=4)
 
