0e266d3ba7a23a24aa0c89e6da8b7774a5e14469
James Collins <collijk@uw.edu>
2020-07-20 01:09:17 -0700

    Finish incorporating splice into forecast

--- src/covid_model_seiir_pipeline/paths.py
+++ src/covid_model_seiir_pipeline/paths.py
@@ -44,7 +44,8 @@ class Paths:
 class ScenarioPaths(Paths):
     """Local directory structure of a forecasting data root."""
     beta_scaling_file: ClassVar[str] = DRAW_FILE_TEMPLATE
-    component_draw_file: ClassVar[str] = DRAW_FILE_TEMPLATE
+    component_file: ClassVar[str] = DRAW_FILE_TEMPLATE
+    outputs_file: ClassVar[str] = DRAW_FILE_TEMPLATE
 
     @property
     def beta_scaling(self) -> Path:
@@ -56,18 +57,27 @@ class ScenarioPaths(Paths):
         return self.beta_scaling / self.beta_scaling_file.format(draw_id=draw_id)
 
     @property
-    def component_draws(self) -> Path:
+    def components(self) -> Path:
         """Folders by location with SEIIR components."""
         return self.root_dir / 'component_draws'
 
-    def get_component_draws_path(self, draw_id: int) -> Path:
+    def get_components_path(self, draw_id: int) -> Path:
         """Get SEIIR components for a particular location and draw."""
-        return self.component_draws / self.component_draw_file.format(draw_id=draw_id)
+        return self.components / self.component_file.format(draw_id=draw_id)
+
+    @property
+    def outputs(self) -> Path:
+        """Cases, deaths, and effective R."""
+        return self.root_dir / 'outputs'
+
+    def get_outputs_path(self, draw_id: int) -> Path:
+        """Single draw of forecast outputs."""
+        return self.outputs / self.outputs_file.format(draw_id=draw_id)
 
     @property
     def directories(self) -> List[Path]:
         """Returns all top level sub-directories."""
-        return [self.beta_scaling, self.component_draws]
+        return [self.beta_scaling, self.components]
 
 
 @dataclass
@@ -86,8 +96,11 @@ class ForecastPaths(Paths):
     def get_beta_scaling_path(self, draw_id: int, scenario: str) -> Path:
         return self.scenario_paths[scenario].get_beta_scaling_path(draw_id)
 
-    def get_component_draws_path(self, draw_id: int, scenario: str) -> Path:
-        return self.scenario_paths[scenario].get_component_draws_path(draw_id)
+    def get_components_path(self, draw_id: int, scenario: str) -> Path:
+        return self.scenario_paths[scenario].get_components_path(draw_id)
+
+    def get_outputs_path(self, draw_id: int, scenario: str) -> Path:
+        return self.scenario_paths[scenario].get_outputs_path(draw_id)
 
     def make_dirs(self):
         for scenario_paths in self.scenario_paths.values():
