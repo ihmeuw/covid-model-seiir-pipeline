6c6a7682c256f151d1f19d6a881543a947960553
James Collins <collijk@uw.edu>
2020-08-11 22:04:16 -0700

    Add loading of output data

--- src/covid_model_seiir_pipeline/forecasting/task/postprocessing.py
+++ src/covid_model_seiir_pipeline/forecasting/task/postprocessing.py
@@ -1,9 +1,10 @@
 from argparse import ArgumentParser, Namespace
-
+import functools
 import logging
+import multiprocessing
 from pathlib import Path
 import shlex
-from typing import Optional
+from typing import Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -21,6 +22,44 @@ def run_seir_postprocessing(forecast_version: str) -> None:
     )
     data_interface = ForecastDataInterface.from_specification(forecast_spec)
 
+    for scenario in forecast_spec.scenarios:
+        infections, deaths, r_effective = load_output_data(scenario, data_interface)
+
+
+
+
+def load_output_data(scenario: str, data_interface: ForecastDataInterface):
+    _runner = functools.partial(
+        load_output_data_by_draw,
+        scenario=scenario,
+        data_interface=data_interface,
+    )
+    draws = range(data_interface.get_n_draws())
+    with multiprocessing.Pool(30) as pool:
+        outputs =  list(pool.map(_runner, draws))
+
+    infections = []
+    deaths = []
+    r_effective = []
+    for draw_id, draw_df in outputs:
+        draw_df = draw_df.set_index(['location_id', 'date']).sort_index()
+        infections.append(draw_df['infections'].rename(f'draw_{draw_id}'))
+        deaths.append(draw_df['deaths'].rename(f'draw_{draw_id}'))
+        r_effective.append(draw_df['r_effective'].rename(f'draw_{draw_id}'))
+    infections = pd.concat(infections, axis=1)
+    deaths = pd.concat(deaths, axis=1)
+    r_effective = pd.concat(r_effective, axis=1)
+
+    return infections, deaths, r_effective
+
+
+
+
+
+
+
+def load_output_data_by_draw(draw_id: int, scenario: str, data_interface: ForecastDataInterface) -> Tuple[int, pd.DataFrame]:
+    return draw_id, data_interface.load_outputs(scenario, draw_id)
 
 
 
