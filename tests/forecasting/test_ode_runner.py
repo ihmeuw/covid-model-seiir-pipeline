8e95f828aa3ed4691761be6132e48e423a3e9684
James Collins <collijk@uw.edu>
2020-09-13 12:16:21 -0700

    Update imports in tests

--- tests/forecasting/test_ode_runner.py
+++ tests/forecasting/test_ode_runner.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from covid_model_seiir_pipeline.forecasting.model import ODERunner, SeiirModelSpecs
+from covid_model_seiir_pipeline.forecasting.model.ode_forecast import _ODERunner, _SeiirModelSpecs
 
 
 def test_ode_runner():
-    specs = SeiirModelSpecs(
+    specs = _SeiirModelSpecs(
         alpha=0.9,
         sigma=1.0,
         gamma1=0.3,
@@ -18,6 +18,6 @@ def test_ode_runner():
     t = np.arange(0, 31, 1)
     beta = 2*np.exp(-0.01*t)
     theta = np.zeros_like(beta)
-    ode_runner = ODERunner('RK45', specs)
+    ode_runner = _ODERunner('RK45', specs)
     result = ode_runner.get_solution(init_cond, t, beta, theta)
     print("Okay!")
