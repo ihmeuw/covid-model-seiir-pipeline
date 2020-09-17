bffa42c1b69535917b520fb43513b992fe7328cf
James Collins <collijk@uw.edu>
2020-09-17 09:54:04 -0700

    Fix test

--- tests/forecasting/test_ode_runner.py
+++ tests/forecasting/test_ode_runner.py
@@ -12,12 +12,13 @@ def test_ode_runner():
         N=100,
         delta=0.1,
     )
+    model = 'old_theta'
     # init_cond = [S, E, I1, I2, R] at t = 0
     init_cond = np.array([96, 0, 2, 2, 0], dtype=float)
 
     t = np.arange(0, 31, 1)
     beta = 2*np.exp(-0.01*t)
     theta = np.zeros_like(beta)
-    ode_runner = _ODERunner('RK45', specs)
+    ode_runner = _ODERunner('RK45', model, specs)
     result = ode_runner.get_solution(init_cond, t, beta, theta)
     print("Okay!")
