cc4dcb580480ce05cd3dee13a1833f64f7527332
James Collins <collijk@uw.edu>
2020-11-18 14:32:12 -0800

    update ode test

--- tests/forecasting/test_ode_runner.py
+++ tests/forecasting/test_ode_runner.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-from covid_model_seiir_pipeline.forecasting.model.ode_forecast import _ODERunner, _SeiirModelSpecs
+from covid_model_seiir_pipeline.forecasting.model.ode_forecast import (
+    _ODERunner,
+    _SeiirModelSpecs,
+    ScenarioSpecification,
+    CompartmentInfo
+)
 
 
 def test_ode_runner():
@@ -12,6 +17,11 @@ def test_ode_runner():
         N=100,
         delta=0.1,
     )
+    scenario_spec = ScenarioSpecification()
+    compartment_info = CompartmentInfo(
+        compartments=['S', 'E', 'I1', 'I2', 'R'],
+        group_suffixes=[],
+    )
     model = 'normal'
     # init_cond = [S, E, I1, I2, R] at t = 0
     init_cond = np.array([96, 0, 2, 2, 0], dtype=float)
@@ -19,6 +29,7 @@ def test_ode_runner():
     t = np.arange(0, 31, 1)
     beta = 2*np.exp(-0.01*t)
     theta = np.zeros_like(beta)
-    ode_runner = _ODERunner('RK45', model, specs)
-    result = ode_runner.get_solution(init_cond, t, beta, theta)
+    params = np.vstack([beta, theta]).T
+    ode_runner = _ODERunner(specs, scenario_spec, compartment_info, parameters=['beta', 'theta'])
+    result = ode_runner.get_solution(init_cond, t, params)
     print("Okay!")
