b29f2dea849d065788c67c6655d70b8bf30efd5c
James Collins <collijk@uw.edu>
2020-12-07 19:32:50 -0800

    Use init subclass instead of abc

--- src/covid_model_seiir_pipeline/workflow_tools/specification.py
+++ src/covid_model_seiir_pipeline/workflow_tools/specification.py
@@ -12,7 +12,7 @@ DEFAULT_QUEUE = 'd.q'
 _TaskSpecDict = Dict[str, Union[str, int]]
 
 
-class TaskSpecification(abc.ABC):
+class TaskSpecification:
     """Validation wrapper for specifying task execution parameters.
 
     This class is intended to outline the parameter space for task
@@ -26,9 +26,16 @@ class TaskSpecification(abc.ABC):
     the values in the `validate` method of this class.
 
     """
+    # Class variables meant to be overridden by subclasses.
     default_max_runtime_seconds: int
     default_m_mem_free: str
     default_num_cores: int
+
+    # Constants for all tasks.
+    _runtime_bounds = [100, 60 * 60 * 24]
+    _mem_re = re.compile('\\d+[G]')
+    _mem_bounds_gb = [1, 1000]
+    _num_core_bounds = [1, 30]
 
     def __init__(self, task_specification_dict: _TaskSpecDict):
         self.name = self.__class__.__name__
@@ -45,26 +52,48 @@ class TaskSpecification(abc.ABC):
             logger.warning(f'Unknown task options specified for {self.name}: {task_specification_dict}. '
                            f'These options will be ignored.')
 
+    def __init_subclass__(cls, **kwargs):
+        name = cls.__name__
+
+        for attr in ['default_max_runtime_seconds', 'default_m_mem_free', 'default_num_cores']:
+            if getattr(cls, attr) is None:
+                raise AttributeError(f'No default value provided for {attr} for task template subclass {name}. '
+                                     'Check your class definition.')
+
+        if not cls._runtime_bounds[0] <= cls.default_max_runtime_seconds <= cls._runtime_bounds[1]:
+            raise AttributeError(f'Invalid default max runtime for {name}: {cls.default_max_runtime_seconds}. '
+                                 f'Max runtime must be in the range {cls._runtime_bounds}.'
+                                 'Check your class definition.')
+
+        if not cls._mem_re.match(cls.default_m_mem_free):
+            raise AttributeError('Memory request is expected to be in the format "XG" where X is an integer. '
+                                 f'You provided {cls.default_m_mem_free} for {name}.')
+        if not cls._mem_bounds_gb[0] <= int(cls.default_m_mem_free[:-1]) <= cls._mem_bounds_gb[1]:
+            raise AttributeError(f'Invalid default max memory for {name}: {cls.default_m_mem_free}. ',
+                                 f'Max memory must be in the range {cls._mem_bounds_gb}G. '
+                                 'Check your class definition.')
+
+        if not cls._num_core_bounds[0] <= cls.default_num_cores <= cls._num_core_bounds[1]:
+            raise AttributeError(f'Invalid num cores for {name}: {cls.default_num_cores}. '
+                                 f'Num cores must be in the range {cls._num_core_bounds}l '
+                                 'Check your class definition.')
+
     def validate(self):
         """Checks specification against some baseline constraints."""
-        runtime_bounds = [100, 60*60*24]
-        if not runtime_bounds[0] <= self.max_runtime_seconds <= runtime_bounds[1]:
+        if not self._runtime_bounds[0] <= self.max_runtime_seconds <= self._runtime_bounds[1]:
             raise ValueError(f'Invalid max runtime for {self.name}: {self.max_runtime_seconds}. '
-                             f'Max runtime must be in the range {runtime_bounds}.')
+                             f'Max runtime must be in the range {self._runtime_bounds}.')
 
-        mem_re = re.compile('\\d+[G]')
-        mem_bounds_gb = [1, 1000]
-        if not mem_re.match(self.m_mem_free):
+        if not self._mem_re.match(self.m_mem_free):
             raise ValueError('Memory request is expected to be in the format "XG" where X is an integer. '
                              f'You provided {self.m_mem_free} for {self.name}.')
-        if not mem_bounds_gb[0] <= int(self.m_mem_free[:-1]) <= mem_bounds_gb[1]:
+        if not self._mem_bounds_gb[0] <= int(self.m_mem_free[:-1]) <= self._mem_bounds_gb[1]:
             raise ValueError(f'Invalid max memory for {self.name}: {self.m_mem_free}. ',
-                             f'Max memory must be in the range {mem_bounds_gb}G.')
+                             f'Max memory must be in the range {self._mem_bounds_gb}G.')
 
-        num_core_bounds = [1, 30]
-        if not num_core_bounds[0] <= self.num_cores <= num_core_bounds[1]:
+        if not self._num_core_bounds[0] <= self.num_cores <= self._num_core_bounds[1]:
             raise ValueError(f'Invalid num cores for {self.name}: {self.num_cores}. '
-                             f'Num cores must be in the range {num_core_bounds}')
+                             f'Num cores must be in the range {self._num_core_bounds}')
 
     def to_dict(self) -> Dict[str, Union[str, int]]:
         """Coerce the specification to a dict for display or write to disk."""
