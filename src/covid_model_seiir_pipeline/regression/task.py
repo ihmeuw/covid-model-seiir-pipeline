44116d47d5f33ba43b3c18ee71b12230073d91e9
James Collins <collijk@uw.edu>
2020-12-08 18:27:55 -0800

    Revert regression coefficient settings to production

--- src/covid_model_seiir_pipeline/regression/task.py
+++ src/covid_model_seiir_pipeline/regression/task.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import shlex
 from typing import Optional
 
+from covid_shared.cli_tools.logging import configure_logging_to_terminal
 import numpy as np
 import pandas as pd
 
