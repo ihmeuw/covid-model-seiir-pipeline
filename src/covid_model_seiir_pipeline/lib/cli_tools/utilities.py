9c05be0ce7f1b26f1689e72b5104e20f279b5b5b
James Collins <collijk@uw.edu>
2020-12-27 18:47:48 -0800

    Add log parser

---
+++ src/covid_model_seiir_pipeline/lib/cli_tools/utilities.py
@@ -0,0 +1,25 @@
+from bdb import BdbQuit
+import functools
+from typing import Any, Callable
+
+
+def handle_exceptions(func: Callable, logger: Any, with_debugger: bool) -> Callable:
+    """Drops a user into an interactive debugger if func raises an error."""
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (BdbQuit, KeyboardInterrupt):
+            raise
+        except Exception as e:
+            logger.exception("Uncaught exception {}".format(e))
+            if with_debugger:
+                import pdb
+                import traceback
+                traceback.print_exc()
+                pdb.post_mortem()
+            else:
+                raise
+
+    return wrapped
