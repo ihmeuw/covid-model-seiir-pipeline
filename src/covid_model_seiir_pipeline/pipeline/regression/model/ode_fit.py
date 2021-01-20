5bb6122ccb86dfb5902e29af4397b0e30b7192a8
James Collins <collijk@uw.edu>
2021-01-20 01:15:51 -0800

    Optimize regression ode

--- src/covid_model_seiir_pipeline/pipeline/regression/model/ode_fit.py
+++ src/covid_model_seiir_pipeline/pipeline/regression/model/ode_fit.py
@@ -38,61 +38,62 @@ def run_beta_fit(df, alpha, sigma, gamma1, gamma2, day_shift,
     t = (df['date'] - df['date'].min()).dt.days.values
     obs = df['infections_draw'].values
 
-    init_cond = {
-        'S': N,
-        'E': obs[0],
-        'I1': 1.0,
-        'I2': 0.0,
-        'R': 0.0
-    }
+    S = math.solve_ode(
+        system=linear_first_order,
+        t=t,
+        init_cond=N - obs[0] - (obs[0] / 5.0) ** (1.0 / alpha),
+        params=np.vstack([
+            [0.] * len(t),
+            -obs,
+        ]),
+        dt=solver_dt,
+    ).ravel()
 
-    # ode solver setup
-    t_params = np.arange(np.min(t), np.max(t) + solver_dt, solver_dt)
+    E = math.solve_ode(
+        system=linear_first_order,
+        t=t,
+        init_cond=obs[0],
+        params=np.vstack([
+            [sigma] * len(t),
+            obs,
+        ]),
+        dt=solver_dt,
+    ).ravel()
 
-    rhs_newE = math.linear_interpolate(t_params, t, obs)
-    # fit the E
-    e_sys = LinearFirstOrder(sigma, solver_class, solver_dt)
-    E = e_sys.simulate(t_params,
-                       np.array([init_cond['E']]),
-                       t_params,
-                       rhs_newE[None, :])[0]
-    E_test = math.solve_ode(linear_first_order, t, obs[0], np.vstack([[sigma] * len(t), obs]), solver_dt)
-    import pdb; pdb.set_trace()
-    # fit I1
-    # modify initial condition of I1
-    init_cond.update({
-        'I1': (rhs_newE[0]/5.0)**(1.0/alpha)
-    })
-    i1_sys = LinearFirstOrder(gamma1, solver_class, solver_dt)
-    I1 = i1_sys.simulate(t_params,
-                         np.array([init_cond['I1']]),
-                         t_params,
-                         sigma*E[None, :])[0]
+    I1 = math.solve_ode(
+        system=linear_first_order,
+        t=t,
+        init_cond=(obs[0] / 5.0) ** (1.0 / alpha),
+        params=np.vstack([
+            [gamma1] * len(t),
+            sigma * E,
+        ]),
+        dt=solver_dt,
+    ).ravel()
 
-    # fit I2
-    i2_sys = LinearFirstOrder(gamma2, solver_class, solver_dt)
-    I2 = i2_sys.simulate(t_params,
-                         np.array([init_cond['I2']]),
-                         t_params,
-                         gamma1*I1[None, :])[0]
+    I2 = math.solve_ode(
+        system=linear_first_order,
+        t=t,
+        init_cond=0.,
+        params=np.vstack([
+            [gamma2] * len(t),
+            gamma1 * I1,
+        ]),
+        dt=solver_dt,
+    ).ravel()
 
-    # fit S
-    init_cond.update({
-        'S': N - init_cond['E'] - init_cond['I1']
-    })
-    s_sys = LinearFirstOrder(0., solver_class, solver_dt)
-    S = s_sys.simulate(t_params,
-                       np.array([init_cond['S']]),
-                       t_params,
-                       -rhs_newE[None, :])[0]
+    R = math.solve_ode(
+        system=linear_first_order,
+        t=t,
+        init_cond=0.,
+        params=np.vstack([
+            [0.] * len(t),
+            gamma2 * I2,
+        ]),
+        dt=solver_dt,
+    ).ravel()
+
     neg_S_idx = S < 0.0
-
-    # fit R
-    r_sys = LinearFirstOrder(0., solver_class, solver_dt)
-    R = r_sys.simulate(t_params,
-                       np.array([init_cond['R']]),
-                       t_params,
-                       gamma2*I2[None, :])[0]
 
     if np.any(neg_S_idx):
         id_min = np.min(np.arange(S.size)[neg_S_idx])
@@ -111,25 +112,15 @@ def run_beta_fit(df, alpha, sigma, gamma1, gamma2, day_shift,
     }
 
     # get beta
-    params = (rhs_newE / ((S / N) * (I1 + I2)**alpha))[None, :]
-
-    params = math.linear_interpolate(t, t_params, params)
-    components = {
-        'newE': math.linear_interpolate(t, t_params, rhs_newE),
-        'newE_obs': math.linear_interpolate(t, t, obs),
-        **{c: math.linear_interpolate(t, t_params, components[c])
-           for c in components},
-    }
+    params = (obs / ((S / N) * (I1 + I2)**alpha))[None, :]
 
     beta_fit = pd.DataFrame({
         'location_id': loc_id,
         'date': date,
         'days': t,
-        'beta': params[0]
+        'beta': params[0],
+        **components
     })
-
-    for k, v in components.items():
-        beta_fit[k] = v
 
     dates = pd.DataFrame({
         'location_id': loc_id,
@@ -146,20 +137,3 @@ def linear_first_order(t: float, y: np.ndarray, p: np.ndarray):
     x = y[0]
     dx = -c * x + f
     return np.array([dx])
-
-class LinearFirstOrder:
-
-    def __init__(self, c, solver_class, solver_dt):
-        self.c = c
-        self.solver = solver_class(self.system, solver_dt)
-
-    def system(self, t, y, p):
-        f = p[0]
-        x = y[0]
-        dx = -self.c*x + f
-        return np.array([dx])
-
-    def simulate(self, t, init_cond, t_params, params):
-        t = np.sort(np.unique(np.array(t)))
-        return self.solver.solve(t, init_cond, t_params, params)
-
