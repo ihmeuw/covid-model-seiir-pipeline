7c2162530804b1f3142662f25467efd5cfa0c3e0
James Collins <collijk@uw.edu>
2021-02-11 09:30:35 -0800

    Last night's config for prod.

--- src/covid_model_seiir_pipeline/pipeline/postprocessing/model/combiners.py
+++ src/covid_model_seiir_pipeline/pipeline/postprocessing/model/combiners.py
@@ -6,12 +6,12 @@ INFECTION_TO_CASE = 11
 
 
 def make_ifr(infections: pd.DataFrame, deaths: pd.DataFrame) -> pd.DataFrame:
-    return deaths / infections.groupby('location_id').shift(-INFECTION_TO_DEATH)
+    return deaths / infections.groupby('location_id').shift(INFECTION_TO_DEATH)
 
 
 def make_ihr(infections: pd.DataFrame, hospital_admissions: pd.DataFrame) -> pd.DataFrame:
-    return hospital_admissions / infections.groupby('location_id').shift(-INFECTION_TO_ADMISSION)
+    return hospital_admissions / infections.groupby('location_id').shift(INFECTION_TO_ADMISSION)
 
 
 def make_idr(infections: pd.DataFrame, cases: pd.DataFrame) -> pd.DataFrame:
-    return cases / infections.groupby('location_id').shift(-INFECTION_TO_CASE)
+    return cases / infections.groupby('location_id').shift(INFECTION_TO_CASE)
