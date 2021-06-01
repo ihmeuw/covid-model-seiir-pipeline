2985de48605715493e80d0c1dd20e41ebf729aae
James Collins <collijk@uw.edu>
2021-06-01 15:59:02 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     install_requirements = [
         'click',
-        'covid_shared>=1.8.0',
+        'covid_shared>=1.9.0',
         'fastparquet',
         'loguru',
         'matplotlib',
