1523f288421b75588f4b4869d26b8109eca99236
James Collins <collijk@uw.edu>
2021-06-01 18:34:22 -0700

    Try this canned solution

--- src/covid_model_seiir_pipeline/pipeline/diagnostics/model/plotter.py
+++ src/covid_model_seiir_pipeline/pipeline/diagnostics/model/plotter.py
@@ -544,7 +544,6 @@ class Plotter:
         for plot_version in self._plot_versions:
             try:
                 data = plot_version.load_output_summaries(measure, self._loc_id)
-                data = data[(start <= data['date']) & (data['date'] <= end)]
             except FileNotFoundError:  # No data for this version, so skip.
                 continue
             data[['mean', 'upper', 'lower']] = transform(data[['mean', 'upper', 'lower']])
@@ -554,6 +553,7 @@ class Plotter:
                 ax.fill_between(data['date'], data['upper'], data['lower'], alpha=FILL_ALPHA, color=plot_version.color)
 
         self.format_date_axis(ax, start, end)
+        autoscale(ax)
 
         if label is not None:
             ax.set_ylabel(label, fontsize=AX_LABEL_FONTSIZE)
@@ -666,3 +666,70 @@ def write_or_show(fig, plot_file: str):
 def make_legend_handles(plot_versions: List[PlotVersion]):
     handles = [mlines.Line2D([], [], color=pv.color, label=pv.label, linewidth=2.5) for pv in plot_versions]
     return handles
+
+############################################################################################################
+# Axis autoscale code included from https://gist.github.com/thomasaarholt/c8440b132aaea9f71f0588af486ad457 #
+############################################################################################################
+
+
+def autoscale(ax=None, axis='y', margin=0.1):
+    '''Autoscales the x or y axis of a given matplotlib ax object
+    to fit the margins set by manually limits of the other axis,
+    with margins in fraction of the width of the plot
+
+    Defaults to current axes object if not specified.
+    '''
+    import matplotlib.pyplot as plt
+    import numpy as np
+    if ax is None:
+        ax = plt.gca()
+    newlow, newhigh = np.inf, -np.inf
+
+    for artist in ax.collections + ax.lines:
+        x, y = get_xy(artist)
+        if axis == 'y':
+            setlim = ax.set_ylim
+            lim = ax.get_xlim()
+            fixed, dependent = x, y
+        else:
+            setlim = ax.set_xlim
+            lim = ax.get_ylim()
+            fixed, dependent = y, x
+
+        low, high = calculate_new_limit(fixed, dependent, lim)
+        newlow = low if low < newlow else newlow
+        newhigh = high if high > newhigh else newhigh
+
+    margin = margin * (newhigh - newlow)
+
+    setlim(newlow - margin, newhigh + margin)
+
+
+def calculate_new_limit(fixed, dependent, limit):
+    '''Calculates the min/max of the dependent axis given
+    a fixed axis with limits
+    '''
+    if len(fixed) > 2:
+        mask = (fixed > limit[0]) & (fixed < limit[1])
+        window = dependent[mask]
+        low, high = window.min(), window.max()
+    else:
+        low = dependent[0]
+        high = dependent[-1]
+        if low == 0.0 and high == 1.0:
+            # This is a axhline in the autoscale direction
+            low = np.inf
+            high = -np.inf
+    return low, high
+
+
+def get_xy(artist):
+    '''Gets the xy coordinates of a given artist
+    '''
+    if "Collection" in str(artist):
+        x, y = artist.get_offsets().T
+    elif "Line" in str(artist):
+        x, y = artist.get_xdata(), artist.get_ydata()
+    else:
+        raise ValueError("This type of object isn't implemented yet")
+    return x, y
