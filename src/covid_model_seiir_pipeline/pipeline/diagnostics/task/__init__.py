baf96089e91dab7ab48ea4b2f3a381a27833c21a
James Collins <collijk@uw.edu>
2021-08-07 10:51:59 -0700

    Fix import path

--- src/covid_model_seiir_pipeline/pipeline/diagnostics/task/__init__.py
+++ src/covid_model_seiir_pipeline/pipeline/diagnostics/task/__init__.py
@@ -1,1 +1,6 @@
-from covid_model_seiir_pipeline.pipeline.diagnostics.task.grid_plots import grid_plots
+from covid_model_seiir_pipeline.pipeline.diagnostics.task.grid_plots import (
+    grid_plots,
+)
+from covid_model_seiir_pipeline.pipeline.diagnostics.task.cumulative_deaths_compare_csv import (
+    cumulative_deaths_compare_csv,
+)
