370f1d41ac55178719dbf44216406762ca20d7fc
James Collins <collijk@uw.edu>
2021-08-07 12:08:02 -0700

    Write all the loading logic

--- src/covid_model_seiir_pipeline/pipeline/diagnostics/task/cumulative_deaths_compare_csv.py
+++ src/covid_model_seiir_pipeline/pipeline/diagnostics/task/cumulative_deaths_compare_csv.py
@@ -1,3 +1,4 @@
+import itertools
 from pathlib import Path
 
 import click
@@ -14,6 +15,9 @@ from covid_model_seiir_pipeline.pipeline.postprocessing import (
 from covid_model_seiir_pipeline.pipeline.diagnostics.specification import (
     DiagnosticsSpecification,
 )
+from covid_model_seiir_pipeline.pipeline.diagnostics.model.pdf_merger import (
+    get_locations_dfs,
+)
 
 logger = cli_tools.task_performance_logger
 
@@ -26,16 +30,32 @@ def run_cumulative_deaths_compare_csv(diagnostics_version: str) -> None:
     )
     cumulative_death_spec = diagnostics_spec.cumulative_deaths_compare_csv
 
+    postprocessing_spec = PostprocessingSpecification.from_path(
+        Path(cumulative_death_spec.comparators[-1].version) / static_vars.POSTPROCESSING_SPECIFICATION_FILE
+    )
+    postprocessing_data_interface = PostprocessingDataInterface.from_specification(postprocessing_spec)
+    hierarchy = postprocessing_data_interface.load_hierarchy()
+    sorted_locs = get_locations_dfs(hierarchy)
+
     data = []
+    max_date = pd.Timestamp('2000-01-01')
 
     for comparator in cumulative_death_spec.comparators:
         postprocessing_spec = PostprocessingSpecification.from_path(
             Path(comparator.version) / static_vars.POSTPROCESSING_SPECIFICATION_FILE
         )
         postprocessing_data_interface = PostprocessingDataInterface.from_specification(postprocessing_spec)
-        for scenario in comparator.scenarios:
-            postprocessing_data_interface.load_output_summaries(scenario, measure='cumulative_deaths')
-            ...
+        full_data = postprocessing_data_interface.load_full_data()
+        max_date = max(max_date, full_data.date.max())
+
+        for (scenario, label), measure in itertools.product(comparator.scenarios.items(), ['deaths', 'unscaled_deaths']):
+            df = postprocessing_data_interface.load_output_summaries(scenario, measure=f'cumulative_{measure}')
+            suffix = '_unscaled' if 'unscaled' in measure else ''
+            name = f'{label}{suffix}'
+            df = df.rename(columns={'mean': name}).set_index(['location_id', 'name'])[name]
+            data.append(df)
+    data = pd.concat(data, axis=1)
+    import pdb; pdb.set_trace()
 
 
     logger.report()
