82d1e6cccad3c9cdc8e32c6c457d21401ad57298
James Collins <collijk@uw.edu>
2021-08-09 18:09:13 -0700

    Make spec a little more flexible

--- src/covid_model_seiir_pipeline/pipeline/diagnostics/specification.py
+++ src/covid_model_seiir_pipeline/pipeline/diagnostics/specification.py
@@ -156,7 +156,9 @@ class DiagnosticsSpecification(utilities.Specification):
     def parse_spec_dict(cls, specification: Dict) -> Tuple:
         data = DiagnosticsData(**specification.get('data', {}))
         workflow = DiagnosticsWorkflowSpecification(**specification.get('workflow', {}))
-        compare_csv = CumulativeDeathsCompareCSVSpecification(**specification.get('cumulative_deaths_compare_csv', None))
+        compare_csv = specification.get('cumulative_deaths_compare_csv', None)
+        if compare_csv:
+            compare_csv = CumulativeDeathsCompareCSVSpecification(**compare_csv)
         grid_plots_configs = specification.get('grid_plots', [])
         grid_plots = [GridPlotsSpecification(**grid_plots_config) for grid_plots_config in grid_plots_configs]
         scatters_configs = specification.get('scatters', [])
