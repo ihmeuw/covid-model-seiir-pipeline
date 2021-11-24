66874cd53a7389696d5c589afc21bd7f53048896
James Collins <collijk@uw.edu>
2021-11-24 08:04:37 -0800

    Fix initial condition

--- src/covid_model_seiir_pipeline/pipeline/fit/model/ode_fit.py
+++ src/covid_model_seiir_pipeline/pipeline/fit/model/ode_fit.py
@@ -139,7 +139,8 @@ def make_initial_condition(parameters: Parameters, full_rates: pd.DataFrame, pop
             loc_initial_condition.loc[loc_start_date, f'S_none{suffix}'] = pop - new_e - infectious
             loc_initial_condition.loc[loc_start_date, f'E_ancestral{suffix}'] = new_e
             loc_initial_condition.loc[loc_start_date, f'NewE_ancestral{suffix}'] = new_e
-            loc_initial_condition.loc[loc_start_date, f'NewE_ancestral_all_{risk_group}'] = new_e
+            loc_initial_condition.loc[loc_start_date, f'NewENaive_ancestral{suffix}'] = new_e
+            loc_initial_condition.loc[loc_start_date, f'infection_ancestral{suffix}'] = new_e
             loc_initial_condition.loc[loc_start_date, f'I_ancestral{suffix}'] = infectious
             for variant in VARIANT_NAMES:
                 loc_initial_condition.loc[:loc_start_date, f'EffectiveSusceptible_{variant}{suffix}'] = pop
