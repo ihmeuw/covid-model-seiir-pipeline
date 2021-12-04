fbe2390d245f010c30185bf54d914d2b1d94d95e
James Collins <collijk@uw.edu>
2021-12-03 17:07:29 -0800

    Module for parallel helpers

---
+++ src/covid_model_seiir_pipeline/lib/parallel.py
@@ -0,0 +1,102 @@
+import functools
+import multiprocessing
+from typing import Any, Callable, List, Optional
+
+import pandas as pd
+import tqdm
+
+
+def load_and_concat_draws(loader: Callable[[int, Optional[List[str]]], pd.DataFrame],
+                          index: pd.Index = None,
+                          column: str = None,
+                          n_draws: int = 1,
+                          num_cores: int = 1,
+                          progress_bar: bool = False) -> pd.DataFrame:
+    """Loads draws of a measure stored in separate files and concats them into a single dataset.
+
+    Parameters
+    ----------
+    loader
+        Function that takes as an argument a draw id and an optional list of columns
+        and loads those columns out of a draw specific dataset.
+    index
+        An optional index used to standardize the loaded dataset when it is loaded.
+        Using a shared index dramatically speeds up concatenation of the draw level
+        datasets.
+    column
+        An optional column to load from the dataset produced by the loader. The returned
+        dataset must be a single column, so this argument can be used to load from a
+        multi-column draw level data set.
+    n_draws
+        The number of draws to load. No bounds checking is done to ensure all draws
+        requested are available to load.
+    num_cores
+        The number of processes to use to load the datasets. If >1, multiprocessing
+        will be used to load the data.
+    progress_bar
+        Whether to display a progress bar when loading the data.
+
+    Returns
+    -------
+    pd.DataFrame
+        A single dataset with a column for each requested draw.
+
+    """
+    _runner = functools.partial(
+        draw_runner,
+        loader=loader,
+        index=index,
+        columns=[column] if column is not None else column,
+    )
+    result = run_parallel(
+        runner=_runner,
+        arg_list=list(range(n_draws)),
+        num_cores=num_cores,
+        progress_bar=progress_bar,
+    )
+    result = pd.concat(result, axis=1)
+    result.columns = [f'draw_{draw}' for draw in range(n_draws)]
+    return result
+
+
+def run_parallel(runner: Callable,
+                 arg_list: List,
+                 num_cores: int,
+                 progress_bar: bool = False) -> List[Any]:
+    """Runs a single argument function in parallel over a list of arguments.
+
+    This function dodges multiprocessing if only a single process is requested to
+    make functions more flexible to debugging. It also supports progress bars if
+    requested.
+
+    """
+    if num_cores == 1:
+        result = []
+        for arg in tqdm.tqdm(arg_list, disable=not progress_bar):
+            result.append(runner(arg))
+    else:
+        with multiprocessing.Pool(num_cores) as pool:
+            result = list(tqdm.tqdm(
+                pool.imap(runner, arg_list),
+                total=len(arg_list),
+                disable=not progress_bar,
+            ))
+    return result
+
+
+def draw_runner(draw_id: int,
+                loader: Callable[[int, Optional[List[str]]], pd.DataFrame],
+                index: Optional[pd.Index],
+                columns: Optional[List[str]]) -> pd.Series:
+    """Loads a draw dataset or a column subset of a draw dataset, and reindexes the dataset if appropriate."""
+    if columns and 'round' in index.names:
+        columns += ['round']
+
+    data = loader(draw_id, columns)
+    if index is not None:
+        data = data.reset_index().set_index(index.names).reindex(index)
+    return data.squeeze()
+
+
+
+
