8b7b2c002e6e610261f1847ba5becdf8a1411217
James Collins <collijk@uw.edu>
2021-12-21 07:18:04 -0800

    Fix compartment math for missing measures

--- src/covid_model_seiir_pipeline/pipeline/fit/model/ode_fit.py
+++ src/covid_model_seiir_pipeline/pipeline/fit/model/ode_fit.py
@@ -357,11 +357,23 @@ def run_ode_fit(initial_condition: pd.DataFrame,
     # but subset to beta in case we only ran a subset of locations for debugging
     full_compartments_diff = full_compartments.groupby('location_id').diff().fillna(full_compartments)
     counts = ode_parameters.base_parameters.filter(like='count').loc[betas.index]
+    no_counts = counts.sum(axis=1, min_count=1).isnull()
     for measure in ['death', 'admission', 'case']:
         no_counts_measure = counts[f'count_all_{measure}'].isnull()
         betas.loc[no_counts_measure, f'beta_{measure}'] = np.nan
         cols = [c for c in full_compartments if measure.capitalize() in c]
-        full_compartments_diff.loc[no_counts_measure, cols] = np.nan
+        full_compartments_diff.loc[no_counts, cols] = np.nan
+
+        # FIXME: Clean this up, we just want to set nan to measures that run out of data.
+        # In the system, we have no beta adjustment factor for the rates when we run
+        # out of counts for a measure but other measures are still present.
+        # Here we carry the last adjustment factor forward in time so we don't end up with
+        # discontinuities in the results.
+        terminal_beta = betas.loc[~no_counts_measure].groupby('location_id').last()
+        measure_ratio = (terminal_beta.loc[:, f'beta_{measure}'] / terminal_beta.loc[:, f'beta']).dropna()
+        if not measure_ratio.empty:  # We have at least some data to adjust on.
+            adjustment_idx = full_compartments.loc[no_counts_measure & ~no_counts].index
+            full_compartments_diff.loc[adjustment_idx, cols] = np.nan
     full_compartments = full_compartments_diff.groupby('location_id').cumsum()
         
     # Can have a composite beta if we don't have measure betas
