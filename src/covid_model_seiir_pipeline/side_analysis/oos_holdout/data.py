d21215953fc2d65c59b23500923946857221ff4e
James Collins <collijk@uw.edu>
2022-01-20 10:35:04 -0800

    Regression task

--- src/covid_model_seiir_pipeline/side_analysis/oos_holdout/data.py
+++ src/covid_model_seiir_pipeline/side_analysis/oos_holdout/data.py
@@ -48,132 +48,42 @@ class OOSHoldoutDataInterface:
     def load_population(self, measure: str) -> pd.DataFrame:
         return self.forecast_data_interface.load_population(measure=measure)
 
-    def load_reported_epi_data(self) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_reported_epi_data()
-
-    def load_hospital_census_data(self) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_hospital_census_data()
-
-    def load_hospital_bed_capacity(self) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_hospital_bed_capacity()
-
-    def load_total_covid_scalars(self, draw_id: int = None) -> pd.DataFrame:
-        specification = self.load_specification()
-        data = self.preprocessing_data_interface.load_total_covid_scalars(draw_id=draw_id)
-        if specification.rates_parameters.mortality_scalar == 'total':
-            return data
-        elif specification.rates_parameters.mortality_scalar == 'unscaled':
-            data.loc[:, :] = 1.0
-            return data
-        else:
-            raise ValueError(f'Unknown scaling option {specification.rates_parameters.mortality_scalar}')
-
-    def load_seroprevalence(self, draw_id: int = None) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_seroprevalence(draw_id=draw_id)
-
-    def load_sensitivity(self, draw_id: int = None) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_sensitivity(draw_id)
-
-    def load_testing_data(self) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_testing_data()
-
-    def load_covariate(self, covariate: str, scenario: str) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_covariate(covariate, scenario)
-
-    def load_covariate_info(self, covariate: str, info_type: str) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_covariate_info(covariate, info_type)
-
-    def load_variant_prevalence(self, scenario: str) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_variant_prevalence(scenario)
-
-    def load_waning_parameters(self, measure: str) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_waning_parameters(measure)
-
-    def load_vaccine_summary(self, columns: List[str] = None) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_vaccine_summary(columns=columns)
-
-    def load_vaccine_uptake(self, scenario: str) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_vaccine_uptake(scenario)
-
-    def load_vaccine_risk_reduction(self, scenario: str) -> pd.DataFrame:
-        return self.preprocessing_data_interface.load_vaccine_risk_reduction(scenario)
-
-    ################
-    # Fit data I/O #
-    ################
-
-    def save_specification(self, specification: FitSpecification) -> None:
-        io.dump(specification.to_dict(), self.fit_root.specification())
-
-    def load_specification(self) -> FitSpecification:
-        spec_dict = io.load(self.fit_root.specification())
-        return FitSpecification.from_dict(spec_dict)
-
-    def save_covariate_options(self, covariate_options: Dict) -> None:
-        io.dump(covariate_options, self.fit_root.covariate_options())
-
-    def load_covariate_options(self, draw_id: int = None) -> Dict:
-        covariate_pool = io.load(self.fit_root.covariate_options())
-        if draw_id is not None:
-            covariate_pool = {rate: draws[draw_id] for rate, draws in covariate_pool.items()}
-        return covariate_pool
-
-    def save_ode_params(self, data: pd.Series, draw_id: int) -> None:
-        io.dump(data, self.fit_root.ode_parameters(draw_id=draw_id))
-
-    def load_ode_params(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.ode_parameters(draw_id=draw_id, columns=columns))
-
-    def save_phis(self, data: pd.Series, draw_id: int) -> None:
-        io.dump(data, self.fit_root.phis(draw_id=draw_id))
-
-    def load_phis(self, draw_id: int) -> pd.DataFrame:
-        return io.load(self.fit_root.phis(draw_id=draw_id))
-
-    def save_input_epi_measures(self, data: pd.DataFrame, draw_id: int):
-        io.dump(data, self.fit_root.input_epi_measures(draw_id=draw_id))
-
-    def load_input_epi_measures(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.input_epi_measures(draw_id=draw_id, columns=columns))
-
-    def save_rates_data(self, data: pd.DataFrame, draw_id: int) -> None:
-        io.dump(data, self.fit_root.rates_data(draw_id=draw_id))
-
-    def load_rates_data(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.rates_data(draw_id=draw_id, columns=columns))
-
-    def save_rates(self, data: pd.DataFrame, draw_id: int) -> None:
-        io.dump(data, self.fit_root.rates(draw_id=draw_id))
-
-    def load_rates(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.rates(draw_id=draw_id, columns=columns))
-
-    def save_posterior_epi_measures(self, data: pd.DataFrame, draw_id: int):
-        io.dump(data, self.fit_root.posterior_epi_measures(draw_id=draw_id))
-
-    def load_posterior_epi_measures(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.posterior_epi_measures(draw_id=draw_id, columns=columns))
-
-    def save_compartments(self, data: pd.DataFrame, draw_id: int) -> None:
-        io.dump(data, self.fit_root.compartments(draw_id=draw_id))
-
-    def load_compartments(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.compartments(draw_id=draw_id, columns=columns))
-
-    def save_fit_beta(self, data: pd.DataFrame, draw_id: int) -> None:
-        io.dump(data, self.fit_root.beta(draw_id=draw_id))
-
     def load_fit_beta(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.beta(draw_id=draw_id, columns=columns))
+        return self.forecast_data_interface.load_fit_beta(draw_id, columns)
 
-    def save_final_seroprevalence(self, data: pd.DataFrame, draw_id: int) -> None:
-        io.dump(data, self.fit_root.seroprevalence(draw_id=draw_id))
+    def load_covariates(self):
+        regression_spec = self.forecast_data_interface.regression_data_interface.load_specification()
+        return self.forecast_data_interface.load_covariates(regression_spec.covariates)
 
-    def load_final_seroprevalence(self, draw_id: int, columns: List[str] = None) -> pd.DataFrame:
-        return io.load(self.fit_root.seroprevalence(draw_id=draw_id, columns=columns))
+    def load_priors(self):
+        regression_spec = self.forecast_data_interface.regression_data_interface.load_specification()
+        return self.forecast_data_interface.regression_data_interface.load_priors(
+            regression_spec.covariates.values(),
+        )
 
-    def save_summary(self, data: pd.DataFrame, measure: str) -> None:
-        io.dump(data, self.fit_root.summary(measure=measure))
+    def load_prior_run_coefficients(self, draw_id: int):
+        return self.forecast_data_interface.load_coefficients(draw_id=draw_id)
 
-    def load_summary(self, measure: str) -> pd.DataFrame:
-        return io.load(self.fit_root.summary(measure=measure))
+    ################
+    # OOS data I/O #
+    ################
+
+    def save_specification(self, specification: OOSHoldoutSpecification) -> None:
+        io.dump(specification.to_dict(), self.oos_root.specification())
+
+    def load_specification(self) -> OOSHoldoutSpecification:
+        spec_dict = io.load(self.oos_root.specification())
+        return OOSHoldoutSpecification.from_dict(spec_dict)
+
+    def save_coefficients(self, coefficients: pd.DataFrame, draw_id: int) -> None:
+        io.dump(coefficients, self.oos_root.coefficients(draw_id=draw_id))
+
+    def load_coefficients(self, draw_id: int) -> pd.DataFrame:
+        return io.load(self.oos_root.coefficients(draw_id=draw_id))
+
+    def save_regression_beta(self, betas: pd.DataFrame, draw_id: int) -> None:
+        io.dump(betas, self.oos_root.beta(draw_id=draw_id))
+
+    def load_regression_beta(self, draw_id: int) -> pd.DataFrame:
+        return io.load(self.oos_root.beta(draw_id=draw_id))
+
---
+++ src/covid_model_seiir_pipeline/side_analysis/oos_holdout/task/regression.py
@@ -0,0 +1,72 @@
+import click
+import numpy as np
+import pandas as pd
+
+from covid_model_seiir_pipeline.lib import (
+    cli_tools,
+    math,
+)
+from covid_model_seiir_pipeline.side_analysis.oos_holdout.data import OOSHoldoutDataInterface
+from covid_model_seiir_pipeline.side_analysis.oos_holdout.specification import OOSHoldoutSpecification
+from covid_model_seiir_pipeline.pipeline.regression import model
+
+
+logger = cli_tools.task_performance_logger
+
+
+def run_oos_holdout_regression(oos_holdout_version: str, draw_id: int) -> None:
+    logger.info('Starting oos regression.', context='setup')
+    # Build helper abstractions
+    specification = OOSHoldoutSpecification.from_version_root(oos_holdout_version)
+    data_interface = OOSHoldoutDataInterface.from_specification(specification)
+
+    logger.info('Loading regression input data', context='read')
+    hierarchy = data_interface.load_hierarchy('pred')
+    beta_fit = data_interface.load_fit_beta(draw_id, columns=['beta', 'round'])
+    beta_fit = beta_fit.loc[(beta_fit['round'] == 2) & (beta_fit['beta'] > 0), 'beta']
+    covariates = data_interface.load_covariates()
+    gaussian_priors = data_interface.load_priors()
+    prior_coefficients = data_interface.load_prior_run_coefficients(draw_id=draw_id)
+    if gaussian_priors and prior_coefficients:
+        raise NotImplementedError
+
+    logger.info('Fitting beta regression', context='compute_regression')
+    holdout_days = specification.parameters.holdout_weeks * 7
+    beta_fit = beta_fit.groupby('location_id').apply(lambda x: x.iloc[:-holdout_days])
+    if specification.parameters.run_regression:
+        regression_spec = data_interface.forecast_data_interface.regression_data_interface.load_specification()
+        coefficients = model.run_beta_regression(
+            beta_fit,
+            covariates,
+            regression_spec.covariates.values(),
+            gaussian_priors,
+            prior_coefficients,
+            hierarchy,
+        )
+    else:
+        coefficients = prior_coefficients
+
+    log_beta_hat = math.compute_beta_hat(covariates, coefficients)
+    beta_hat = np.exp(log_beta_hat).rename('beta_hat')
+
+    # Format and save data.
+    logger.info('Prepping outputs', context='transform')
+    betas = pd.concat([beta_fit, beta_hat], axis=1)
+
+    logger.info('Writing outputs', context='write')
+    data_interface.save_regression_beta(betas, draw_id=draw_id)
+    data_interface.save_coefficients(coefficients, draw_id=draw_id)
+
+    logger.report()
+
+
+@click.command()
+@cli_tools.with_task_regression_version
+@cli_tools.with_draw_id
+@cli_tools.add_verbose_and_with_debugger
+def beta_regression(regression_version: str, draw_id: int,
+                    verbose: int, with_debugger: bool):
+    cli_tools.configure_logging_to_terminal(verbose)
+    run = cli_tools.handle_exceptions(run_beta_regression, logger, with_debugger)
+    run(regression_version=regression_version,
+        draw_id=draw_id)
