64cc9dce4eb6f206b43b7aefe831b0c9dd67fc52
James Collins <collijk@uw.edu>
2022-03-14 01:12:41 -0700

    Add heavy hand death fixes for central/eastern europe

--- src/covid_model_seiir_pipeline/pipeline/fit/model/the_heavy_hand.py
+++ src/covid_model_seiir_pipeline/pipeline/fit/model/the_heavy_hand.py
@@ -173,112 +173,160 @@ def rescale_kappas(measure: str,
         sampled_ode_params['kappa_omicron_admission'] = kappa_omicron_admission
 
     if measure == 'death':
-        ifr_scaling_factors = [
-            # (   34,  5.0),  # Azerbaijan
-            # (   43,  3.0),  # Albania
-            # (   44,  3.0),  # Bosnia and Herzegovina
-            # (   45, 10.0),  # Croatia
-            # (   47,  2.0),  # Czechia
-            # (   50,  5.0),  # Montenegro
-            # (   49,  3.0),  # North Macedonia
-            # (   51,  2.0),  # Poland
-            # (   53,  2.0),  # Serbia
-            # (   57,  3.0),  # Belarus
-            # (   59,  2.0),  # Latvia
-            # (   60,  2.0),  # Lithuania
-            # (   62,  3.0),  # Russian Federation
-            # (  349,  5.0),  # Greenland
-            # (  535,  2.0),  # Idaho
-            # ( 3539,  2.0),  # Spokane County
-            # (  573,  2.0),  # Wyoming
-            # (   78,  2.0),  # Denmark
-            # (35498,  2.0),  # Provincia autonoma di Bolzano
-            # (   90,  2.0),  # Norway
-            # (  122,  2.0),  # Ecuador
-            # (  113,  3.0),  # Guyana
-            # (  118,  3.0),  # Suriname
-            # ( 4644,  2.0),  # Baja California
-            # ( 4645,  2.0),  # Baja California Sur
-            # ( 4649,  0.2),  # Chiapas
-            # ( 4647,  2.0),  # Coahuila
-            # ( 4652,  2.0),  # Durango
-            # ( 4653,  2.0),  # Guanajuato
-            # ( 4654,  0.5),  # Guerrero
-            # ( 4651,  2.0),  # Mexico City
-            # ( 4661,  2.0),  # Nuevo Leon
-            # ( 4665,  2.0),  # Quintana Roo
-            # ( 4670,  2.0),  # Tamaulipas
-            # ( 4672,  0.5),  # Veracruz de Ignacio de la Llave
-            # ( 4674,  2.0),  # Zacatecas
-            # (  133,  0.2),  # Venezuela
-            # ( 4751,  0.2),  # Alagoas
-            # ( 4754,  0.5),  # Bahia
-            # ( 4756,  0.5),  # Distrito Federal
-            # ( 4758,  0.5),  # Goias
-            # ( 4760,  0.5),  # Minas Gerais
-            # ( 4763,  0.5),  # Para
-            # ( 4764,  0.5),  # Paraiba
-            # ( 4765,  0.5),  # Parana
-            # ( 4766,  0.5),  # Pernambuco
-            # ( 4767,  0.2),  # Piaui
-            # ( 4769,  0.5),  # Rio Grande do Norte
-            # ( 4768,  0.2),  # Rio de Janeiro
-            # ( 4770,  0.5),  # Rondonia
-            # ( 4773,  0.5),  # Santa Catarina
-            # ( 4774,  0.5),  # Sergipe
-            # ( 4776,  0.5),  # Tocantins
-            # (  136,  3.0),  # Paraguay
-            # (  139,  0.5),  # Algeria
-            # (  143,  0.5),  # Iraq
-            # (  144,  2.0),  # Jordan
-            # (  146,  3.0),  # Lebanon
-            # (  147,  2.0),  # Libya
-            # (  149,  3.0),  # Palestine
-            # (  152,  0.5),  # Saudi Arabia
-            # (  522,  2.0),  # Sudan
-            # (  154,  3.0),  # Tunisia
-            # (  155,  2.0),  # Turkey
-            # ( 4849,  3.0),  # Delhi
-            # ( 4852,  2.0),  # Haryana
-            # ( 4861,  2.0),  # Manipur
-            # ( 4862,  2.0),  # Meghalaya
-            # ( 4863,  5.0),  # Mizoram
-            # ( 4865,  2.0),  # Odisha
-            # ( 4871,  0.5),  # Telangana
-            # ( 4872,  3.0),  # Tripura
-            # ( 4874,  2.0),  # Uttarakhand
-            # ( 4875,  2.0),  # West Bengal
-            # (  168,  5.0),  # Angola
-            # (  171,  2.0),  # Democratic Republic of the Congo
-            # (  172,  3.0),  # Equatorial Guinea
-            # (  176,  0.2),  # Comoros
-            # (  179,  3.0),  # Ethiopia
-            # (  180,  3.0),  # Kenya
-            # (  181,  2.0),  # Madagascar
-            # (  182,  3.0),  # Malawi
-            # (  184,  3.0),  # Mozambique
-            # (  187,  0.2),  # Somalia
-            # (  190,  2.0),  # Uganda
-            # (  194,  3.0),  # Lesotho
-            # (  195,  2.0),  # Namibia
-            # (  198,  3.0),  # Zimbabwe
-            # (  201,  3.0),  # Burkina Faso
-            # (  205,  3.0),  # Cote d'Ivoire
-            # (  206,  3.0),  # Gambia
-            # (  208,  3.0),  # Guinea
-            # (  211,  3.0),  # Mali
-            # (  212,  2.0),  # Mauritania
-            # (  215,  5.0),  # Sao Tome and Principe
-            # (  216,  2.0),  # Senegal
-        ]
-        kappa_omicron_death = pd.Series(
-            sampled_ode_params['kappa_omicron_death'],
-            index=compartments.reset_index().location_id.unique(),
-            name='kappa_omicron_death'
-        )
+        ifr_scaling_factors = {
+            'alpha': [
+                (34, 3.0),  # Azerbaijan
+                (43, 3.0),  # Albania
+                (44, 3.0),  # Bosnia and Herzegovina
+                (45, 3.0),  # Bulgaria
+                (49, 3.0),  # North Macedonia
+                (57, 3.0),  # Belarus
+            ],
+            'delta': [
+                (33, 3.0),  # Armenia
+                (34, 3.0),  # Azerbaijan
+                (35, 3.0),  # Georgia
+                (36, 3.0),  # Kazakhstan
+                (38, 1.5),  # Mongolia
+                (41, 1.5),  # Uzbekistan
+                (43, 3.0),  # Albania
+                (44, 3.0),  # Bosnia and Herzegovina
+                (45, 3.0),  # Bulgaria
+                (50, 3.0),  # Montenegro
+                (49, 3.0),  # North Macedonia
+                (57, 3.0),  # Belarus
+                (59, 3.0),  # Latvia
+                (60, 3.0),  # Lithuania
+            ],
+            'omicron': [
+                (33, 3.0),  # Armenia
+                (34, 3.0),  # Azerbaijan
+                (35, 3.0),  # Georgia
+                (36, 3.0),  # Kazakhstan
+                (38, 1.5),  # Mongolia
+                (41, 1.5),  # Uzbekistan
+                (43, 3.0),  # Albania
+                (44, 3.0),  # Bosnia and Herzegovina
+                (45, 3.0),  # Bulgaria
+                (46, 1.5),  # Croatia
+                (47, 1.5),  # Czechia
+                (48, 1.5),  # Hungary
+                (50, 3.0),  # Montenegro
+                (49, 3.0),  # North Macedonia
+                (52, 1.5),  # Romania
+                (57, 3.0),  # Belarus
+                (59, 3.0),  # Latvia
+                (60, 3.0),  # Lithuania
+                (61, 1.5),  # Moldova
+
+            ],
+        }
+        # (   34,  5.0),  # Azerbaijan
+        # (   43,  3.0),  # Albania
+        # (   44,  3.0),  # Bosnia and Herzegovina
+        # (   45, 10.0),  # Croatia
+        # (   47,  2.0),  # Czechia
+        # (   50,  5.0),  # Montenegro
+        # (   49,  3.0),  # North Macedonia
+        # (   51,  2.0),  # Poland
+        # (   53,  2.0),  # Serbia
+        # (   57,  3.0),  # Belarus
+        # (   59,  2.0),  # Latvia
+        # (   60,  2.0),  # Lithuania
+        # (   62,  3.0),  # Russian Federation
+        # (  349,  5.0),  # Greenland
+        # (  535,  2.0),  # Idaho
+        # ( 3539,  2.0),  # Spokane County
+        # (  573,  2.0),  # Wyoming
+        # (   78,  2.0),  # Denmark
+        # (35498,  2.0),  # Provincia autonoma di Bolzano
+        # (   90,  2.0),  # Norway
+        # (  122,  2.0),  # Ecuador
+        # (  113,  3.0),  # Guyana
+        # (  118,  3.0),  # Suriname
+        # ( 4644,  2.0),  # Baja California
+        # ( 4645,  2.0),  # Baja California Sur
+        # ( 4649,  0.2),  # Chiapas
+        # ( 4647,  2.0),  # Coahuila
+        # ( 4652,  2.0),  # Durango
+        # ( 4653,  2.0),  # Guanajuato
+        # ( 4654,  0.5),  # Guerrero
+        # ( 4651,  2.0),  # Mexico City
+        # ( 4661,  2.0),  # Nuevo Leon
+        # ( 4665,  2.0),  # Quintana Roo
+        # ( 4670,  2.0),  # Tamaulipas
+        # ( 4672,  0.5),  # Veracruz de Ignacio de la Llave
+        # ( 4674,  2.0),  # Zacatecas
+        # (  133,  0.2),  # Venezuela
+        # ( 4751,  0.2),  # Alagoas
+        # ( 4754,  0.5),  # Bahia
+        # ( 4756,  0.5),  # Distrito Federal
+        # ( 4758,  0.5),  # Goias
+        # ( 4760,  0.5),  # Minas Gerais
+        # ( 4763,  0.5),  # Para
+        # ( 4764,  0.5),  # Paraiba
+        # ( 4765,  0.5),  # Parana
+        # ( 4766,  0.5),  # Pernambuco
+        # ( 4767,  0.2),  # Piaui
+        # ( 4769,  0.5),  # Rio Grande do Norte
+        # ( 4768,  0.2),  # Rio de Janeiro
+        # ( 4770,  0.5),  # Rondonia
+        # ( 4773,  0.5),  # Santa Catarina
+        # ( 4774,  0.5),  # Sergipe
+        # ( 4776,  0.5),  # Tocantins
+        # (  136,  3.0),  # Paraguay
+        # (  139,  0.5),  # Algeria
+        # (  143,  0.5),  # Iraq
+        # (  144,  2.0),  # Jordan
+        # (  146,  3.0),  # Lebanon
+        # (  147,  2.0),  # Libya
+        # (  149,  3.0),  # Palestine
+        # (  152,  0.5),  # Saudi Arabia
+        # (  522,  2.0),  # Sudan
+        # (  154,  3.0),  # Tunisia
+        # (  155,  2.0),  # Turkey
+        # ( 4849,  3.0),  # Delhi
+        # ( 4852,  2.0),  # Haryana
+        # ( 4861,  2.0),  # Manipur
+        # ( 4862,  2.0),  # Meghalaya
+        # ( 4863,  5.0),  # Mizoram
+        # ( 4865,  2.0),  # Odisha
+        # ( 4871,  0.5),  # Telangana
+        # ( 4872,  3.0),  # Tripura
+        # ( 4874,  2.0),  # Uttarakhand
+        # ( 4875,  2.0),  # West Bengal
+        # (  168,  5.0),  # Angola
+        # (  171,  2.0),  # Democratic Republic of the Congo
+        # (  172,  3.0),  # Equatorial Guinea
+        # (  176,  0.2),  # Comoros
+        # (  179,  3.0),  # Ethiopia
+        # (  180,  3.0),  # Kenya
+        # (  181,  2.0),  # Madagascar
+        # (  182,  3.0),  # Malawi
+        # (  184,  3.0),  # Mozambique
+        # (  187,  0.2),  # Somalia
+        # (  190,  2.0),  # Uganda
+        # (  194,  3.0),  # Lesotho
+        # (  195,  2.0),  # Namibia
+        # (  198,  3.0),  # Zimbabwe
+        # (  201,  3.0),  # Burkina Faso
+        # (  205,  3.0),  # Cote d'Ivoire
+        # (  206,  3.0),  # Gambia
+        # (  208,  3.0),  # Guinea
+        # (  211,  3.0),  # Mali
+        # (  212,  2.0),  # Mauritania
+        # (  215,  5.0),  # Sao Tome and Principe
+        # (  216,  2.0),  # Senegal
+
         if rates_parameters.heavy_hand_fixes:
-            for location_id, ifr_scaling_factor in ifr_scaling_factors:
-                kappa_omicron_death.loc[location_id] *= ifr_scaling_factor
-        sampled_ode_params['kappa_omicron_death'] = kappa_omicron_death
+            for variant, scaling_factors in ifr_scaling_factors.items():
+                kappa = pd.Series(
+                    sampled_ode_params[f'kappa_{variant}_death'],
+                    index=compartments.reset_index().location_id.unique(),
+                    name=f'kappa_{variant}_death'
+                )
+                for location_id, scaling_factor in scaling_factors:
+                    kappa.loc[location_id] *= scaling_factor
+                sampled_ode_params[f'kappa_{variant}_death'] = kappa
 
     return sampled_ode_params
