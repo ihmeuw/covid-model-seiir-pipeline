6b5856b1ad46273d0638758463b671d9a1ee8a1d
James Collins <collijk@uw.edu>
2022-11-16 13:20:39 -0800

    Different fencepost

--- src/covid_model_seiir_pipeline/pipeline/forecasting/task/beta_forecast.py
+++ src/covid_model_seiir_pipeline/pipeline/forecasting/task/beta_forecast.py
@@ -146,26 +146,20 @@ def run_beta_forecast(forecast_version: str, scenario: str, draw_id: int, progre
         hierarchy=hierarchy,
     )
 
-    locations_to_run = list(initial_condition.reset_index().location_id.unique())
-    logger.info(
-        f'Running initial ODE system for {len(locations_to_run)} locations.',
-        context='ODE system'
-    )
-    compartments, chis, failed = model.run_ode_forecast(
-        initial_condition,
-        model_parameters,
-        num_cores=num_cores,
-        progress_bar=progress_bar,
-        location_ids=locations_to_run,
-    )
-
-    reimposition_number = 1
+    reimposition_number = 0
     max_num_reimpositions = scenario_spec.mandate_reimposition['max_num_reimpositions']
-
+    locations_to_run = list(initial_condition.reset_index().location_id.unique())
     while reimposition_number <= max_num_reimpositions and locations_to_run:
         logger.info(
             f'Running ODE system on reimposition {reimposition_number} for {len(locations_to_run)} locations.',
             context='ODE system'
+        )
+        compartments, chis, failed = model.run_ode_forecast(
+            initial_condition,
+            model_parameters,
+            num_cores=num_cores,
+            progress_bar=progress_bar,
+            location_ids=locations_to_run,
         )
 
         reimposition_dates = model.compute_reimposition_dates(
@@ -175,6 +169,10 @@ def run_beta_forecast(forecast_version: str, scenario: str, draw_id: int, progre
             reimposition_threshold=reimposition_threshold,
         )
 
+        locations_to_run = reimposition_dates.index.tolist()
+        if not locations_to_run:
+            break
+
         covariates, min_reimposition_dates = model.reimpose_mandates(
             reimposition_dates=reimposition_dates,
             covariates=covariates,
@@ -183,18 +181,10 @@ def run_beta_forecast(forecast_version: str, scenario: str, draw_id: int, progre
 
         beta, beta_hat = build_beta_final(covariates=covariates)
         model_parameters.base_parameters.loc[:, 'beta_all_infection'] = beta
-
-        locations_to_run = reimposition_dates.index.tolist()
-
-        compartments, chis, failed = model.run_ode_forecast(
-            initial_condition,
-            model_parameters,
-            num_cores=num_cores,
-            progress_bar=progress_bar,
-            location_ids=locations_to_run,
-        )
-
         reimposition_number += 1
+
+
+
 
     system_metrics = model.compute_output_metrics(
         indices=indices,
